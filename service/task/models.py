3d478f3e81687557bc82b11e60faee0aa08d8d5a
Artuom4ik <masterskayaigr123@gmail.com>
2024-06-25 19:14:52 +0300

    Add client and employee in Task model

--- service/task/models.py
+++ service/task/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 from django.utils import timezone
 
+from accounts.models import User
+
 
 class Task(models.Model):
     STATUS_CHOICES = (
         ('pending', 'Ожидает исполнителя'),
         ('in_progress', 'В процессе'),
         ('completed', 'Выполнена'),
+    )
+
+    client = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'client'},
+        related_name='client_task',
+        verbose_name="Заказчик"
+    )
+    employee = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='employee_task',
+        verbose_name="Сотрудник"
     )
 
     created_at = models.DateTimeField(
