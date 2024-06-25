db9552565ae1b420fd091e4e84b796540fb6ae0d
Artuom4ik <masterskayaigr123@gmail.com>
2024-06-25 19:02:34 +0300

    Create custom User model

--- service/accounts/models.py
+++ service/accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(User):
+    ROLE = {
+        ("employee", "Сотрудник"),
+        ("client", "Заказчик")
+    }
+    middle_name = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name="Отчество"
+    )
+    phone_number = PhoneNumberField(
+        blank=True,
+        region="BY",
+        verbose_name="Номер телефона"
+    )
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE,
+        verbose_name="Роль"
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}: {self.role}'
